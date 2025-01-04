896f8d023a2ae53db9194659b60ab5f145cb09db
alenaKaliuta <lenkazasloveskya@gmail.com>
2025-01-04 23:57:14 +0300

    feat: 27 passing

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
+Math.LN2
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const activity = Number(sampleActivity);
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  if (Number.isNaN(activity)) {
+    return false;
+  }
+  if (Math.sign(activity) < 1) {
+    return false;
+  }
+  const decayConstant = Math.LN2 / HALF_LIFE_PERIOD;
+  const age = Math.log(MODERN_ACTIVITY / activity) / decayConstant;
+  return (age > 0) ? Math.ceil(age) : false;
 }
 
 module.exports = {
--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const str1Arr = s1.split('').sort((a, b) => a.localeCompare(b));
+  const str2Arr = s2.split('').sort((a, b) => a.localeCompare(b));
+  let result = 0;
+  let i = 0;
+  while (i < str1Arr.length) {
+    const arr1 = str1Arr.filter(elem => elem === str1Arr[i]);
+    const arr2 = str2Arr.filter(elem => elem === str1Arr[i]);
+    const charNumber = (arr1.length <= arr2.length) ? arr1.length : arr2.length;
+    result += charNumber;
+    i += arr1.length;
+  }
+  return result;
 }
 
 module.exports = {
