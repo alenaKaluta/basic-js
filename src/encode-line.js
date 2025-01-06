c1c323782e23d487c401b62c159657c7132d9512
alenaKaliuta <lenkazasloveskya@gmail.com>
2025-01-06 18:25:02 +0300

    feat: 47 passing

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let result = '';
+  let i = 0;
+  while (i < str.length) {
+    let number = 1;
+    for (let j = i + 1; j < str.length; j += 1) {
+      if (str[i] !== str[j]) {
+        break
+      } else {
+        number += 1;
+      }
+    }
+    if (number === 1) {
+      result += `${str[i]}`;
+    } else {
+      result += `${number}${str[i]}`;
+    }
+    i += number;
+  }
+  return result;
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const condition1 =  options.addition || typeof options.addition === 'boolean';
+  const condition2 =  options.addition || typeof options.addition === 'object';
+  if (condition1 || condition2) {
+    let additionStr = options.addition;
+    if (options.additionRepeatTimes > 1) {
+      let additionPart;
+      if (options.additionSeparator) {
+        additionPart = `${options.additionSeparator}${additionStr}`;
+      } else {
+        additionPart = `|${additionStr}`;
+      }
+      for (let i = 1; i < options.additionRepeatTimes; i += 1) {
+        additionStr += additionPart;
+      }
+    }
+    str = `${str}${additionStr}`;
+  }
+  let result = str;
+  for (let j = 1; j < options.repeatTimes; j += 1) {
+    if (options.separator) {
+      result += `${options.separator}${str}`;
+    } else {
+      result += `+${str}`;
+    }
+  }
+  return result;
 }
 
 module.exports = {
