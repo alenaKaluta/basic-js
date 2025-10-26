78df9f6e1f0c4898fdc91a5158698b5cd3423a63
alenaKaliuta <lenkazasloveskya@gmail.com>
2025-10-26 21:54:14 +0300

    feat: count-cats passing

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  return matrix.reduce((acc, item) => {
+    if (item.includes('^^')) {
+      const catsInElem = item.filter((elem)=> elem === '^^');
+      const catsNumInElem = catsInElem.length;
+      acc += catsNumInElem;
+    }
+    return acc;
+  }, 0);
 }
 
 module.exports = {
